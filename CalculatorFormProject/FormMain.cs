2fc5a4edcd1280aff10fc708782cde3125e78c96
Stud. Michele Ghisolfi <m.ghisolfi.1318@vallauri.net>
2020-09-25 12:03:37 +0200

    Numbers, comma, plus-minus sign

--- CalculatorFormProject/FormMain.cs
+++ CalculatorFormProject/FormMain.cs
@@ -16,10 +16,17 @@ namespace CalculatorFormProject
         {
             public char Content;
             public bool IsBold;
-            public ButtonStruct(char content, bool isBold)
+            public bool IsNumber;
+            public bool IsDeciamlSeparator;
+            public bool IsPlusMinusSign;
+            public ButtonStruct(char content, bool isBold,
+                bool isNumber = false, bool isDecimalSeparator = false, bool isPlusMinusSign = false)
             {
                 this.Content = content;
                 this.IsBold = isBold;
+                this.IsNumber = isNumber;
+                this.IsDeciamlSeparator = isDecimalSeparator;
+                this.IsPlusMinusSign = isPlusMinusSign;
             }
             public override string ToString()
             {
@@ -31,11 +38,11 @@ namespace CalculatorFormProject
         private ButtonStruct[,] buttons =
         {
             { new ButtonStruct('%', false), new ButtonStruct(' ', false), new ButtonStruct('C',false), new ButtonStruct(' ',false) },
-            { new ButtonStruct('⅟', false), new ButtonStruct(' ', false), new ButtonStruct(' ',false), new ButtonStruct('/',false) },
-            { new ButtonStruct('7', true), new ButtonStruct('8', true), new ButtonStruct('9',true), new ButtonStruct('x',false) },
-            { new ButtonStruct('4', true), new ButtonStruct('5', true), new ButtonStruct('6',true), new ButtonStruct('-',false) },
-            { new ButtonStruct('1', true), new ButtonStruct('2', true), new ButtonStruct('3',true), new ButtonStruct('+',false) },
-            { new ButtonStruct('±', false), new ButtonStruct('0', true), new ButtonStruct(',',false), new ButtonStruct('=',false) },
+            { new ButtonStruct(' ', false, false), new ButtonStruct(' ', false), new ButtonStruct(' ',false), new ButtonStruct('/',false) },
+            { new ButtonStruct('7', true, true), new ButtonStruct('8', true, true), new ButtonStruct('9',true, true), new ButtonStruct('x',false) },
+            { new ButtonStruct('4', true, true), new ButtonStruct('5', true, true), new ButtonStruct('6',true, true), new ButtonStruct('-',false) },
+            { new ButtonStruct('1', true, true), new ButtonStruct('2', true, true), new ButtonStruct('3',true, true), new ButtonStruct('+',false) },
+            { new ButtonStruct('±', false, false, false, true), new ButtonStruct('0', true, true), new ButtonStruct(',',false, false, true), new ButtonStruct('=',false) },
         };
 
         private RichTextBox resultBox;
@@ -46,11 +53,11 @@ namespace CalculatorFormProject
 
         private void FormMain_Load(object sender, EventArgs e)
         {
-            MakeResultBox(resultBox);
+            MakeResultBox();
             MakeButtons(buttons);
         }
 
-        private void MakeResultBox(RichTextBox resultBox)
+        private void MakeResultBox()
         {
             resultBox = new RichTextBox();
             resultBox.SelectionAlignment = HorizontalAlignment.Right;
@@ -59,6 +66,8 @@ namespace CalculatorFormProject
             resultBox.Height = 50;
             resultBox.Top = 20;
             resultBox.ReadOnly = true;
+            resultBox.Text = "0";
+            resultBox.TabStop = false;
             this.Controls.Add(resultBox);
         }
 
@@ -87,12 +96,50 @@ namespace CalculatorFormProject
                     newButton.Height = buttonHeight;
                     newButton.Left = posX;
                     newButton.Top = posY;
+                    newButton.Tag = bs; // taggato con ButtonStruct relativa
+                    newButton.Click += Button_Click;
                     this.Controls.Add(newButton);
                     posX += buttonWidth;
                 }
                 posX = 0;
                 posY += buttonHeight;
             }
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            ButtonStruct bs = (ButtonStruct)clickedButton.Tag;
+            if (bs.IsNumber)
+            {
+                if (resultBox.Text == "0")
+                {
+                    resultBox.Text = "";
+                }
+                resultBox.Text += clickedButton.Text;
+            }
+            else
+            {
+                if (bs.IsDeciamlSeparator)
+                {
+                    if (!resultBox.Text.Contains(bs.Content))
+                    {
+                        resultBox.Text += clickedButton.Text;
+                    }
+                }
+                if (bs.IsPlusMinusSign)
+                {
+                    if (!resultBox.Text.Contains("-"))
+                    {
+                        resultBox.Text = "-" + resultBox.Text;
+                    }
+                    else
+                    {
+                        resultBox.Text = resultBox.Text.Replace("-", "");
+                    }
+                }
+            }
+
         }
     }
 }

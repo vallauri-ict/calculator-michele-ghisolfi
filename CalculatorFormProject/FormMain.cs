c35b3b3d4e7ac22bd0efd9ff168f37ba47922add
Stud. Michele Ghisolfi <m.ghisolfi.1318@vallauri.net>
2020-10-07 11:16:38 +0200

    Formattazione result
    TODO: gestire bene cifre dopo la virgola.

--- CalculatorFormProject/FormMain.cs
+++ CalculatorFormProject/FormMain.cs
@@ -1,6 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CalculatorFormProject
@@ -12,7 +18,7 @@ namespace CalculatorFormProject
             public char Content;
             public bool IsBold;
             public bool IsNumber;
-            public bool IsDeciamlSeparator;
+            public bool IsDecimalSeparator;
             public bool IsPlusMinusSign;
             public bool IsOperator;
             public bool IsEqualSign;
@@ -23,7 +29,7 @@ namespace CalculatorFormProject
                 this.Content = content;
                 this.IsBold = isBold;
                 this.IsNumber = isNumber;
-                this.IsDeciamlSeparator = isDecimalSeparator;
+                this.IsDecimalSeparator = isDecimalSeparator;
                 this.IsPlusMinusSign = isPlusMinusSign;
                 this.IsOperator = isOperator;
                 this.IsEqualSign = isEqualSign;
@@ -37,15 +43,16 @@ namespace CalculatorFormProject
         // private char[,] buttons = new char[6, 4];
         private ButtonStruct[,] buttons =
         {
-            { new ButtonStruct('%', false), new ButtonStruct(' ', false), new ButtonStruct('C',false), new ButtonStruct('<',false) },
-            { new ButtonStruct(' ', false, false), new ButtonStruct(' ', false), new ButtonStruct(' ',false), new ButtonStruct('/',false, false, false, false, true) },
-            { new ButtonStruct('7', true, true), new ButtonStruct('8', true, true), new ButtonStruct('9',true, true), new ButtonStruct('x',false, false, false, false, true) },
-            { new ButtonStruct('4', true, true), new ButtonStruct('5', true, true), new ButtonStruct('6',true, true), new ButtonStruct('-',false, false, false, false, true) },
-            { new ButtonStruct('1', true, true), new ButtonStruct('2', true, true), new ButtonStruct('3',true, true), new ButtonStruct('+',false, false, false, false, true) },
-            { new ButtonStruct('±', false, false, false, true), new ButtonStruct('0', true, true), new ButtonStruct(',',false, false, true), new ButtonStruct('=',false, false, false, false, true, true) }
+             { new ButtonStruct(' ', false), new ButtonStruct(' ', false), new ButtonStruct('C', false), new ButtonStruct('<', false) },
+                { new ButtonStruct(' ', false), new ButtonStruct(' ', false), new ButtonStruct(' ', false), new ButtonStruct('/', false, false, false, false, true) },
+                { new ButtonStruct('7', true, true), new ButtonStruct('8', true, true), new ButtonStruct('9', true, true), new ButtonStruct('x', false, false, false, false, true) },
+                { new ButtonStruct('4', true, true), new ButtonStruct('5', true, true), new ButtonStruct('6', true, true), new ButtonStruct('-', false, false, false, false, true) },
+                { new ButtonStruct('1', true, true), new ButtonStruct('2', true, true), new ButtonStruct('3', true, true), new ButtonStruct('+', false, false, false, false, true) },
+                { new ButtonStruct('±', false, false, false, true), new ButtonStruct('0', true, true), new ButtonStruct(',', false, false, true), new ButtonStruct('=', false, false, false, false, true, true) }
         };
 
         private RichTextBox resultBox;
+        private Font baseFont = new Font("Segoe UI", 22, FontStyle.Bold);
 
         private const char ASCIIZERO = '\x0000';
         private double operand1, operand2, result;
@@ -67,7 +74,7 @@ namespace CalculatorFormProject
         {
             resultBox = new RichTextBox();
             resultBox.SelectionAlignment = HorizontalAlignment.Right;
-            resultBox.Font = new Font("Segoe UI", 22);
+            resultBox.Font = baseFont;
             resultBox.Width = this.Width - 16;
             resultBox.Height = 50;
             resultBox.Top = 20;
@@ -80,11 +87,21 @@ namespace CalculatorFormProject
 
         private void ResultBox_TextChanged(object sender, EventArgs e)
         {
-            int newSize = 22 + (15 - resultBox.Text.Length);
-            if (newSize > 8 && newSize < 23)
+            if (resultBox.Text.Length == 1)
             {
-                int delta = 15 - resultBox.Text.Length;
-                resultBox.Font = new Font("Segoe UI", newSize);
+                resultBox.Font = baseFont;
+            }
+            else
+            {
+                int delta = 17 - resultBox.Text.Length;
+                if (delta % 2 == 0)
+                {
+                    float newSize = baseFont.Size + delta;
+                    if (newSize > 8 && newSize < 23)
+                    {
+                        resultBox.Font = new Font(baseFont.FontFamily, newSize, baseFont.Style);
+                    }
+                }
             }
         }
 
@@ -137,15 +154,15 @@ namespace CalculatorFormProject
                 {
                     resultBox.Text = "";
                 }
-                resultBox.Text += clickedButton.Text;
+                if (resultBox.Text.Length < 20) resultBox.Text += clickedButton.Text;
             }
             else
             {
-                if (bs.IsDeciamlSeparator)
+                if (bs.IsDecimalSeparator)
                 {
                     if (!resultBox.Text.Contains(bs.Content))
                     {
-                        resultBox.Text += clickedButton.Text;
+                        if (resultBox.Text.Length < 20) resultBox.Text += clickedButton.Text;
                     }
                 }
                 if (bs.IsPlusMinusSign)
@@ -182,13 +199,19 @@ namespace CalculatorFormProject
             lastButtonClicked = bs;
         }
 
+        private string getFormattedNumber(double number)
+        {
+            return String.Format("{0:0,0.00000}", number);
+            // return number.ToString("N", CultureInfo.InvariantCulture);	
+        }
+
         private void clearAll(double numberToWrite = 0)
         {
             operand1 = 0;
             operand2 = 0;
             result = 0;
             lastOperator = ASCIIZERO;
-            resultBox.Text = numberToWrite.ToString();
+            resultBox.Text = getFormattedNumber(numberToWrite);
         }
 
         private void manageOperators(ButtonStruct bs)
@@ -221,14 +244,17 @@ namespace CalculatorFormProject
                         case '/':
                             result = operand1 / operand2;
                             break;
+                        default:
+                            break;
                     }
+                    operand1 = result;
                     if (!bs.IsEqualSign)
                     {
                         lastOperator = bs.Content;
                         operand2 = 0;
                     }
-                    operand1 = result;
-                    resultBox.Text = result.ToString();
+                    
+                    resultBox.Text = getFormattedNumber(result);
                 }
             }
         }
